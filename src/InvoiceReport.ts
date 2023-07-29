a216da4b51eb6f387c103017c4eee22eab38df4d
Anuj Chauhan <anujc@Anujs-MacBook-Air.local>
2023-07-29 13:58:05 +0530

    refactored the code to generate report

--- src/InvoiceReport.ts
+++ src/InvoiceReport.ts
@@ -13,35 +13,53 @@ export class InvoiceReport {
         this.monthlyUsageByCompanyMap = this.billingFormat.parse(monthlyUsageByCompany);
     }
 
-    generageBillingForOpenSeatings(numberOfOpenSeatings: number) {
-        return 5000 * numberOfOpenSeatings * (1 + this.GSTRate);
+    generateChargeForOpenSeatings(numberOfOpenSeatings: number) {
+        return 5000 * numberOfOpenSeatings;
     }
 
-    generateBillingForCabinSeatings(numberOfCabinSeatings: number) {
-        let billing = 10000 * numberOfCabinSeatings * (1 + this.GSTRate);
-        return billing;
+    generageGSTForOpenSeatings(numberOfOpenSeatings: number) {
+        return 5000 * numberOfOpenSeatings * this.GSTRate;
+    }
+
+    private generateChargeForCabinSeatings(numberOfCabinSeatings: number) {
+        return 10000 * numberOfCabinSeatings;
+    }
+
+    private generageGSTForCabinSeatings(numberOfCabinSeatings: number) {
+        return 10000 * numberOfCabinSeatings * this.GSTRate;
     }
 
     generateBillingReport(): string {
         let billingReport:string = '';
+        let totalBilling:number = 0;
+        let totalGST:number = 0;
         let index:number = 0;
+
         for (let key in this.monthlyUsageByCompanyMap) {
             if(index > 0 ){
                 billingReport += '\n';
             }
             if (key === 'Open Seats') {
-                let numberOfOpenSeatings = this.monthlyUsageByCompanyMap[key];
-                let billing = this.generageBillingForOpenSeatings(numberOfOpenSeatings);
-                billingReport += `${numberOfOpenSeatings} Open Seats : ${billing}`;
+                let numberOfOpenSeatings:number = this.monthlyUsageByCompanyMap[key];
+                let charge:number = this.generateChargeForOpenSeatings(numberOfOpenSeatings);
+                let gstCharge: number = this.generageGSTForOpenSeatings(numberOfOpenSeatings);
+                billingReport += `${numberOfOpenSeatings} Open Seats: ${charge + gstCharge}`;
+                totalBilling += charge + gstCharge;
+                totalGST += gstCharge;
             }
             else{
-                let numberOfCabinSeatings = this.monthlyUsageByCompanyMap[key];
-                let billing = this.generateBillingForCabinSeatings(numberOfCabinSeatings);
-                billingReport += `${numberOfCabinSeatings} Cabin Seats : ${billing}`;
+                let numberOfCabinSeatings:number = this.monthlyUsageByCompanyMap[key];
+                let charge:number = this.generateChargeForCabinSeatings(numberOfCabinSeatings);
+                let gstCharge: number = this.generageGSTForCabinSeatings(numberOfCabinSeatings);
+                billingReport += `${numberOfCabinSeatings} Cabin Seats: ${charge + gstCharge}`;
+                totalBilling += charge + gstCharge;
+                totalGST += gstCharge;
             }
-
             index++;
         }
+        billingReport += `\nTotal: ${totalBilling}`;
+        billingReport += `\nTotal GST: ${totalGST}`;
+
         return billingReport;
     }
 
--- tests/InvoiceReport.spec.ts
+++ tests/InvoiceReport.spec.ts
@@ -6,45 +6,71 @@ describe('Invoice Report Tests', function () {
         'when X number of Open Seatings are booked by a client company.', function () {
         let monthlyUsageByCompany = "2 Open Seats"
         let GSTRate = 0.18;
-        let expectedBillingReport = monthlyUsageByCompany + ' : ' + `${5000 * 2 * (1 + GSTRate)}`;
+        let expectedBillingReport = monthlyUsageByCompany + ': ' + `${5000 * 2 * (1 + GSTRate)}`
+                                            + '\nTotal: ' + `${5000 * 2 * (1 + GSTRate)}`
+                                            + '\nTotal GST: ' + `${5000 * 2 * GSTRate}`;
+
         let billingFormat = new BillingFormat();
 
         // Act
         let invoiceReport = new InvoiceReport(monthlyUsageByCompany, billingFormat, GSTRate);
-        let actualBilling = invoiceReport.generateBillingReport();
+        let actualBillingReport = invoiceReport.generateBillingReport();
 
         // Assert
-        expect(actualBilling).toBe(expectedBillingReport);
+        expect(actualBillingReport).toBe(expectedBillingReport);
     });
 
     it('should generate a billing of `10000 * X * (1 + GSTRate)` ' +
         'when X number of Cabin Seatings are booked by a client company.', function () {
         let monthlyUsageByCompany = "3 Cabin Seats"
         let GSTRate = 0.18;
-        let expectedBillingReport = monthlyUsageByCompany + ' : ' + `${10000 * 3 * (1 + GSTRate)}`;
+        let expectedBillingReport = monthlyUsageByCompany + ': ' + `${10000 * 3 * (1 + GSTRate)}`
+                                            + '\nTotal: ' + `${10000 * 3 * (1 + GSTRate)}`
+                                            + '\nTotal GST: ' + `${10000 * 3 * GSTRate}`;
+
         let billingFormat = new BillingFormat();
 
         // Act
         let invoiceReport = new InvoiceReport(monthlyUsageByCompany, billingFormat, GSTRate);
-        let actualBilling = invoiceReport.generateBillingReport();
+        let actualBillingReport = invoiceReport.generateBillingReport();
 
         // Assert
-        expect(actualBilling).toBe(expectedBillingReport);
+        expect(actualBillingReport).toBe(expectedBillingReport);
     });
 
 
     it('should generate a combined billing report for Open Seatings and Cabin Seatings', function () {
         let monthlyUsageByCompany = "2 Open Seats\n3 Cabin Seats"
         let GSTRate = 0.18;
-        let expectedBillingReport = '2 Open Seats : 11800' + '\n' + '3 Cabin Seats : 35400';
+        let expectedBillingReport = '2 Open Seats: 11800' + '\n' + '3 Cabin Seats: 35400'
+                                        + '\nTotal: ' + `${10000 * 3 * (1 + GSTRate) + 5000 * 2 * (1 + GSTRate)}`
+                                        + '\nTotal GST: ' + `${10000 * 3 * GSTRate + 5000 * 2 * GSTRate}`;
+
         let billingFormat = new BillingFormat();
 
         // Act
         let invoiceReport = new InvoiceReport(monthlyUsageByCompany, billingFormat, GSTRate);
-        let actualBilling = invoiceReport.generateBillingReport();
+        let actualBillingReport = invoiceReport.generateBillingReport();
 
         // Assert
-        expect(actualBilling).toBe(expectedBillingReport);
+        expect(actualBillingReport).toBe(expectedBillingReport);
+    });
+
+
+    it('should generate a combined billing report for Cabin Seating and Open Seating.', function () {
+        let monthlyUsageByCompany = "3 Cabin Seats\n2 Open Seats"
+        let GSTRate = 0.18;
+        let expectedBillingReport = '3 Cabin Seats: 35400' + '\n' + '2 Open Seats: 11800'
+                                        + '\nTotal: ' + `${10000 * 3 * (1 + GSTRate) + 5000 * 2 * (1 + GSTRate)}`
+                                        + '\nTotal GST: ' + `${10000 * 3 * GSTRate + 5000 * 2 * GSTRate}`;
+        let billingFormat = new BillingFormat();
+
+        // Act
+        let invoiceReport = new InvoiceReport(monthlyUsageByCompany, billingFormat, GSTRate);
+        let actualBillingReport = invoiceReport.generateBillingReport();
+
+        // Assert
+        expect(actualBillingReport).toBe(expectedBillingReport);
     });
 
 
